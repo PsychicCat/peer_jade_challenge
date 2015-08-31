95464dc64ed559f8da4833bac58ef63e3b104688
Brendan Telzrow <telzrowb@gmail.com>
2015-08-31 16:35:14 -0500

    finished section 2

--- routes/memes.js
+++ routes/memes.js
@@ -2,45 +2,14 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
+var memes = require('../models/images');
+var messages = require('../models/messages');
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var file = path.join(__dirname, '../models/images.json');
-    fs.readFile(file, 'utf8', function (err, data) {
-        if (err) {
-            next(err);
-        } else {
-            var memes = JSON.parse(data);
-            var memeObject = {meme: memes};
-            res.render('memes', memeObject);
-        }
-
-    });
-    var file2 = path.join(__dirname, '../models/messages.json');
-    fs.readFile(file2, 'utf8', function (err, data) {
-        if (err) {
-            next(err);
-        } else {
-            console.log(data);
-            var messages = JSON.parse(data);
-            var memeObject.messages = messages
-            res.render('memes', {message: messages});
-        }
-
-    });
+    res.render('memes', {memes: memes, messages: messages})
 });
-//router.get('/', function(req, res, next){
-    //var file = path.join(__dirname, '../models/messages.json');
-    //fs.readFile(file, 'utf8', function(err, data){
-    //    if(err){
-    //        next(err);
-    //    }else{
-    //        var messages = JSON.parse(data);
-    //        res.render('memes', {message: messages})
-    //    }
 
-//    });
-//
-//});
 module.exports = router;
+
