9c845e4acffa70e8835a79d80d4b13897a771fa7
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-08-31 15:00:41 -0500

    added messages and file path

--- routes/memes.js
+++ routes/memes.js
@@ -12,10 +12,35 @@ router.get('/', function(req, res, next) {
             next(err);
         } else {
             var memes = JSON.parse(data);
-            res.render('memes', {meme: memes});
+            var memeObject = {meme: memes};
+            res.render('memes', memeObject);
+        }
+
+    });
+    var file2 = path.join(__dirname, '../models/messages.json');
+    fs.readFile(file2, 'utf8', function (err, data) {
+        if (err) {
+            next(err);
+        } else {
+            console.log(data);
+            var messages = JSON.parse(data);
+            var memeObject.messages = messages
+            res.render('memes', {message: messages});
         }
 
     });
 });
+//router.get('/', function(req, res, next){
+    //var file = path.join(__dirname, '../models/messages.json');
+    //fs.readFile(file, 'utf8', function(err, data){
+    //    if(err){
+    //        next(err);
+    //    }else{
+    //        var messages = JSON.parse(data);
+    //        res.render('memes', {message: messages})
+    //    }
 
+//    });
+//
+//});
 module.exports = router;
