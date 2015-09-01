35ee0e74eaff18429ca931ffbf9ede4468c3e0b6
Brendan Telzrow <telzrowb@gmail.com>
2015-09-01 05:58:51 -0500

    finished section 3 added POST route

---
+++ routes/postMessage.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+    var newId = req.body.id;
+    var newMessage = req.body.message;
+    var file = path.join(__dirname, '../models/messages.json');
+
+    fs.readFile(file, 'utf8', function(err, data){
+        if (err) {
+            next(err);
+        } else {
+            if(data){
+                var obj = JSON.parse(data);
+            }else {
+                var obj = [];
+            }
+            obj.push({"message": newMessage, "id": newId});
+
+            fs.writeFile(file, JSON.stringify(obj), function(err){
+                if (err) {
+                    throw err;
+                } else {
+                    console.log('Wrote Data!');
+                }
+            });
+        }
+    })
+});
+
+module.exports = router;
